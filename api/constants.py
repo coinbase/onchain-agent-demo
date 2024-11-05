833f7b7917db388e6d98284be7ad6d4a720a3c01
Alec Chen <93971719+0xAlec@users.noreply.github.com>
2024-11-05 12:49:07 -0500

    add constants

---
+++ api/constants.py
@@ -0,0 +1,8 @@
+# Event types
+EVENT_TYPE_AGENT = "agent"
+EVENT_TYPE_COMPLETED = "completed"
+EVENT_TYPE_TOOLS = "tools"
+EVENT_TYPE_ERROR = "error"
+
+# Environment variables
+WALLET_DATA_ENV_VAR = "CDP_WALLET_DATA"
